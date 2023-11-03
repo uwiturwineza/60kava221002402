4ddac90144cd8bd83940977ae66e6d154e91e4ee
uwiturwineza <149577253+uwiturwineza@users.noreply.github.com>
2023-11-03 11:24:32 +0200

    Add files via upload
    class work

---
+++ myfirstproject/diva.java
@@ -0,0 +1,16 @@
+ class diva {
+public static void main(String args[])
+{
+    int a=10;
+    int b=15;
+    if(a>b)
+    {
+        system.out.println("a is greater");
+    } 
+        else{
+            system.out.println("b is greater");
+        }
+    }
+
+
+}
